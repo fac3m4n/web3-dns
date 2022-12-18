13104c057501cebd774d861aa94a2a2b39808188
Kerim <kerimberdi.agayev@edusoftech.eu>
2022-12-18 16:25:52 +0200

    domain contract and tests

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,39 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+
+    // mapping datatype to store domains
+    mapping (string => address) public domains;
+
+    //values mapping
+    mapping (string => string) records;    
     constructor() {
         console.log("My first contract");
     }
+
+    // register func to add names to mapping
+    function register(string calldata name) public {
+
+        // check if name registered
+        require(domains[name]==address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain", msg.sender);
+    }
+
+    // get domain owner address
+    function getAddress(string calldata name) view public returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // check if sender is owner
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) view public returns (string memory) {
+        return records[name];
+    }
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,20 @@
 const main = async () => {
+    const [owner, randomAddress] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    const transaction = await domainContract.register("doom");
+    await transaction.wait();
+
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Owner of Domain:", domainOwner);
+
+    // Trying to set a record that doesn't belong to me!
+    txn = await domainContract.connect(randomAddress).setRecord("doom", "Haha my domain now!");
+    await txn.wait();
 
 };
 
